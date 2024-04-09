c96b8d3d9c252a96021eacb101a6f3e04ec926b7
Ryan Slocum <ryanslo161@gmail.com>
2024-04-09 15:27:53 -0600

    updating evaluation scripts

--- evaluation_tools/compare.py
+++ evaluation_tools/compare.py
@@ -1,7 +1,9 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
 from evaluate_rpe import read_trajectory, evaluate_trajectory
+import os
 
 # Comparison tools
     # https://github.com/weichnn/Evaluation_Tools
@@ -9,19 +11,15 @@ from evaluate_rpe import read_trajectory, evaluate_trajectory
 
 
 
-def clean_data(results_dir, dso_traj_name):
+def clean_GT(results_dir):
     with open(f"{results_dir}/poses.csv", 'r') as gt_file, \
-        open(f"{results_dir}/{dso_traj_name}.txt", 'r') as dso_file, \
-        open(f"{results_dir}/times.txt", 'r') as times_file:
-
+            open(f"{results_dir}/times.txt", 'r') as times_file:
+        
         # Read the poses and times
         gt_reader = csv.reader(gt_file)
         next(gt_reader, None) # Skip this line: "# each line is a 3x4 column-major matrix"
         next(gt_reader, None) # Skip this line: "# representing the camera to world transform "
         gt_poses = [list(map(float, row)) for row in gt_reader]
-
-        dso_lines = dso_file.readlines()
-        dso_poses = [list(map(float, line.split())) for line in dso_lines]
 
         times_reader = csv.reader(times_file, delimiter=' ')
         times = [row[1] for row in times_reader]
@@ -44,10 +42,15 @@ def clean_data(results_dir, dso_traj_name):
                 # Write to csv file
                 gt_traj_writer.writerow(gt_pose)
                 # print("GT Pose:", gt_pose)
+
+def clean_data(results_dir, dso_traj_name):
+    with open(f"{results_dir}/{dso_traj_name}.txt", 'r') as dso_file:
+        dso_lines = dso_file.readlines()
+        dso_poses = [list(map(float, line.split())) for line in dso_lines]
                 
         with open(f"{results_dir}/{dso_traj_name}.traj", 'w') as dso_traj_file:
             dso_traj_writer = csv.writer(dso_traj_file)
-            #C dso_traj_writer.writerow(["time", "tx", "ty", "tz", "qx", "qy", "qz", "qw"])
+            # dso_traj_writer.writerow(["time", "tx", "ty", "tz", "qx", "qy", "qz", "qw"])
             for i, dso_line in enumerate(dso_poses):
                 dso_traj_writer.writerow(dso_line)
                 # print("DSO Pose:", dso_line)
@@ -56,41 +59,101 @@ def clean_data(results_dir, dso_traj_name):
 
 if __name__ == "__main__":
     # if not os.path.isfile(f"{RESULTS_DIR}/gt_traj.traj") or not os.path.isfile(f"{RESULTS_DIR}/{DSO_TRAJ_NAME}.traj"):
-    RESULTS_DIR = "/Users/ryanslocum/Downloads/run_data/"
+    RESULTS_DIR = "/Users/ryanslocum/Downloads/run_data_transfer/"
+    PLOT_DIR = "/Users/ryanslocum/Documents/class/seniorThesis/repos/lightDSO/evaluation_tools/plots/"
     result_names = [
         "backwards_2min",
         "close_quarters_2min",
         "completely_planar_2min",
-        # "completely_stationary_1min",
         "frequent_stops_2min",
         "mostly_straight_2min",
-        "only_rotation_2min",
         "straight_fullspeed_3min",
         "tricky_2min",
         "turn_around_2min"
     ]
-    for name in result_names:
-        for traj_name in ["dso_baseline_result", "dso_attempt1_result"]:
-            clean_data(f"{RESULTS_DIR}{name}", traj_name)
 
-
-
-    total = 0
+    algo_names = ["dso_result_", "lightDSO_result_a4_"]
+    NUM_EVAL = 5
 
     for name in result_names:
-        rpes = []
+        clean_GT(f"{RESULTS_DIR}{name}")
+        for traj_name in algo_names:
+            for iter in range(NUM_EVAL):
+                print(f"Cleaning data for {name}/{traj_name}{iter}")
+                clean_data(f"{RESULTS_DIR}{name}", f"{traj_name}{iter}")
+
+
+    RPES = {}
+
+    for name in result_names:
         traj_gt = read_trajectory(f"{RESULTS_DIR}{name}/gt_traj.traj")
-        for traj_name in ["dso_baseline_result", "dso_attempt1_result"]:
-            traj_est = read_trajectory(f"{RESULTS_DIR}{name}/{traj_name}.traj")
-            result = evaluate_trajectory(traj_gt,traj_est,param_max_pairs=0)
-            trans_error = np.array(result)[:,4]
-            # print(f"{name}/{traj_name} RPE: {np.mean(trans_error)}")
-            rpes.append(np.mean(trans_error))
+        for traj_name in algo_names:
+            for iter in range(NUM_EVAL):
+                traj_est = read_trajectory(f"{RESULTS_DIR}{name}/{traj_name}{iter}.traj")
+                try:
+                    result = evaluate_trajectory(traj_gt,traj_est,param_max_pairs=0)
+                    trans_error = np.array(result)[:,4]
+                    rpe = np.mean(trans_error)
+                except:
+                    rpe = None
+                RPES[f"{name}/{traj_name}{iter}"] = rpe
+                print(f"Evaluated RPE for {name}/{traj_name}{iter}: {rpe}")
 
 
-        percent_improvement = round(((rpes[0] - rpes[1]) / rpes[0] )* 100 ,2)
-        total += percent_improvement
-        print(f"{name}/{traj_name} RPE percent improvement: {percent_improvement}%")
-        print()
 
-    print(f"Avg percent improvement: {round(total/len(result_names), 2)}%")
+
+    results_grid = []
+
+    for name in result_names:
+        grid = []
+        for baseline in [f"{algo_names[0]}{i}" for i in range(NUM_EVAL)]:
+            baseline_grid = []
+            baseline_rpe = RPES[f"{name}/{baseline}"]
+
+            for modified in [f"{algo_names[1]}{i}" for i in range(NUM_EVAL)]:
+            
+                modified_rpe = RPES[f"{name}/{modified}"]
+                if baseline_rpe is None or modified_rpe is None:
+                    percent_improvement = 0
+                else:
+                    percent_improvement = round(((baseline_rpe - modified_rpe) / baseline_rpe) * 100, 2)
+
+                baseline_grid.append(percent_improvement)
+            grid.append(baseline_grid)
+        results_grid.append(grid)
+
+
+
+
+
+    # Convert the results grid to a numpy array
+    results_array = np.array(results_grid)
+
+    # Create a directory to save the plots
+    os.makedirs(PLOT_DIR + algo_names[1], exist_ok=True)
+
+    # Iterate over each grid
+    for i in range(len(result_names)):
+        # Create the subplot for the current grid
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        # Create the heatmap for the current grid
+        ax.imshow(results_array[i,:,:], cmap='plasma', interpolation='nearest', vmin=-100, vmax=100)
+
+        # Add the numbers to the heatmap
+        for x in range(NUM_EVAL):
+            for y in range(NUM_EVAL):
+                ax.text(y, x, results_array[i, x, y], ha='center', va='center', color='white')
+
+        # Add labels and title to the subplot
+        ax.set_xlabel('Modified Trajectory')
+        ax.set_ylabel('Baseline Trajectory')
+        ax.set_title(f'{result_names[i]}')
+        print(f"Average for {result_names[i]}: {np.mean(results_array[i,:,:])}")
+
+        # Save the plot to the plot directory
+        plot_path = os.path.join(f'{PLOT_DIR + algo_names[1] + "/"}{i}_{result_names[i]}.png')
+        plt.savefig(plot_path)
+
+        # Close the plot
+        plt.close(fig)
